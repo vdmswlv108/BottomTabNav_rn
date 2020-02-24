c9fe040ee711b96a706134cc92c8525579df71af
vdmswlv108 <59062123+vdmswlv108@users.noreply.github.com>
2020-02-24 16:36:29 +0900

    Update Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-export default class MyPage extends Component {
+export default class Search extends Component {
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ fontSize: 20 }}>MyPage</Text>
+                <Text style={{ fontSize: 20 }}>Search</Text>
             </View>
         )
     }
